a73d53f1b09bd6864aade62e67b19015bad689f7
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-09-09 17:44:30 +0300

    Create point_in_triangle.h

---
+++ geometry/src/geometry/point_in_triangle.h
@@ -0,0 +1,8 @@
+#ifndef UNTITLED3_POINT_IN_TRIANGLE_H
+#define UNTITLED3_POINT_IN_TRIANGLE_H
+
+namespace geometry{
+    bool check_point(float x1, float y1, float x2, float y2, float x3, float y3, float x, float y);
+}
+
+#endif //UNTITLED3_POINT_IN_TRIANGLE_H
