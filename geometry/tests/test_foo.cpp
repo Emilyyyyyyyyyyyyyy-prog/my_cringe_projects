7c8248ba885a10cc5253fb6a2f662ad34ba47e7a
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-09-12 09:58:08 +0300

    Update test_foo.cpp

--- geometry/tests/test_foo.cpp
+++ geometry/tests/test_foo.cpp
@@ -1,8 +1,11 @@
 #include <cassert>
-
 #include "geometry/point_in_triangle.h"
 
 int main(){
-    assert(geometry::check_point(0, 0, 1, 1, 1, 0, 0, 0.5));
+    Vector2d v1 = Vector2d(0, 0);
+    Vector2d v2 = Vector2d(1, 1);
+    Vector2d v3 = Vector2d(1, 0);
+    Vector2d v = Vector2d(0, 0.5);
+    assert(geometry::check_point(v1, v2, v3, v));
 }
 
