83d77fabe0917439bb46c6603c48d759476779e5
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2024-09-17 17:25:04 +0300

    Update point_in_triangle.cpp

--- geometry/src/geometry/point_in_triangle.cpp
+++ geometry/src/geometry/point_in_triangle.cpp
@@ -2,14 +2,14 @@
 
 namespace geometry {
     bool check_point(Vector2d v1, Vector2d v2, Vector2d v3, Vector2d v) {
-        float x1 = v1.getX();
-        float y1 = v1.getY();
-        float x2 = v2.getX();
-        float y2 = v2.getY();
-        float x3 = v3.getX();
-        float y3 = v3.getY();
-        float x = v.getX();
-        float y = v.getY();
+        float x1 = v1.x();
+        float y1 = v1.y();
+        float x2 = v2.x();
+        float y2 = v2.y();
+        float x3 = v3.x();
+        float y3 = v3.y();
+        float x = v.x();
+        float y = v.y();
         double a = (x1 - x) * (y2 - y1) - (x2 - x1) * (y1 - y);
         double b = (x2 - x) * (y3 - y2) - (x3 - x2) * (y2 - y);
         double c = (x3 - x) * (y1 - y3) - (x1 - x3) * (y3 - y);
