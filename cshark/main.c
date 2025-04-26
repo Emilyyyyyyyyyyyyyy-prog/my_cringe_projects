0b71d0345913e38be89dadf549198272cb6ea5e7
Emily <58360984+Emilyyyyyyyyyyyyyy-prog@users.noreply.github.com>
2025-04-26 11:10:04 +0300

    Add files via upload

---
+++ cshark/main.c
@@ -0,0 +1,28 @@
+//project"shark"
+#include<stdio.h>
+
+int main()
+        {if(1){ int s1=48,s2=47,s3=46,s4=45,s5=44,s6=20,s7=42,s8=5,s9=30,s10=6,b=0,b1,c;char a=32;for(int i=0;i<s1;i++){
+        printf("%c",a);}printf("/\\\n");for(int i=0;i<s2;i++){printf("%c",a);}printf("/%c%c|\n",a,a);for(int i=0+b;i<s3;
+        i++){printf("%c",a);}printf("/%c%c%c/",a,a,a);for(int i=0;i<s3-10;i++){printf("%c",a);}printf("/\\\n");for(int i
+        =0;i<s4;i++           ){printf(   "%c",a)   ;}printf("/")   ;b1=10;printf           ("%c%c%c|"   ,a,a,a)   ;for(
+        int i=0;i<     s4-9    ;i=i+1){   printf(   "%c",a);}c=b     ;printf("/")   ;b=b1+   0;printf(   "%c/",   a);b1=
+        c;c=0;b=b1;   printf("\n");b=c;   for(int   i=0;i<s5-19   ;   i++){printf   ("%c",a   );} for(   int i   =0;i<s6
+        -1;i=i+1+c){    printf("_");}b=   0;b1=b*   10;printf(   "/"   );for (int   i=0;i<   4;i=i+1){   c=0   ;printf(
+        "%c",a);}b=c+1    ;printf("|");   for(int   i=0;i<s3-   11;i=   i+1){b++;   b1++;   printf("_"         );}printf
+        ("/%c/\n",a);for    (int i=0;i<             s5-s7+23             ;i=i+c+1          ){b=c*100+b   +b1   *2;printf
+        (" ");}printf("\\"   ); for(int   i=0;i<1   +s2;i++)   {printf   ("%c",a)   ;}for   (int i=0;i   <40;   i=i+10){
+        printf("_")   ;b=b+   1;}printf   ("%c",a   );printf   ("%c/",   a); for(   int i=   0;i<s8;i=   i+1){   printf(
+        "-");}b1=0;    c=b    +1;printf   ("\\|")   ;printf(   "\n");b   =c+b1*10   -5;for(   int i=0;   i<26;i   =i+1){
+        printf("%c",         a);}printf   ("\\");   c=b*2-b1   ;printf   ("%c%c",   a,a);b=   0;printf   ("%cO",   a);b=
+        c+1;for(int i=0;i<s7;i++){printf("%c",a);}printf("/");for(int i=0;i<4;i++){printf("%c",a);}printf("\\|\n");b1=b;
+        for(int i=0;i<s5-17;i++){printf("%c",a);}printf("\\");for(int i=0;i<s10;i++){printf("_");}for(int i=0;i<4;i=i+1)
+        {printf("%c|",a);}for(int i=0;i<4;i++){printf("%c",a);}for(int i=0;i<s10;i++){printf("_");}for( int i=0;i<s10;i=
+        i+1){printf("%c",a);}for(int i=0;i<14;i++){printf("_");}printf("/\n");for(int i=0;i<s9-1;i=i+1){printf("%c",a);}
+        printf("\\/%c",a);for(int i=0;i<s8;i++){printf("%c|",a);}printf("%c%c%c/",a,a,a);c=b+b1-10;for(int i=0;i<s10;i++
+        ){printf("%c",a);}printf("\\");for(int i=0;i<s8;i++){printf("%c",a);}printf("\\\n");for(int i=0;i<s9;i++){printf
+        ("%c",a);}printf("__/");for(int i=0;i<11;i++){printf("%c",a);}printf("/");for(int i=0;i<9;i=i+1){printf("%c",a);
+        }printf("\\__");printf("%c%c%c",a,a,a);printf("\\\n");for(int i=0;i<s9;i++){printf("%c",a);}printf("\\");for(int
+        i=0;i<12;i++){printf("_");}printf("/");for(int i=10;i<23;i=i+1){printf("%c",a);}printf("\\__");printf("_\\\n");}
+return 0;
+}
